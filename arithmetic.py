ea82f71457a44df16c35dc1230b29bb203cf8289
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-02 12:35:16 -0700

    Full version works

--- arithmetic.py
+++ arithmetic.py
@@ -3,9 +3,7 @@
 
 def add(num1, num2):
     """Return the sum of the two inputs."""
-
     return num1 + num2
-
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
